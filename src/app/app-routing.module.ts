5a0a3a9c790834c70259dad09ad4fbe85fd711df
yairuth <yairnazareth866@gmail.com>
2023-11-26 12:01:26 -0400

    Repositorio sistema gestion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { PasswordComponent } from './password/password.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', redirectTo: 'login', pathMatch: 'full' },
+   { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "Password", component: PasswordComponent, pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordComponent } from './password/password.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
